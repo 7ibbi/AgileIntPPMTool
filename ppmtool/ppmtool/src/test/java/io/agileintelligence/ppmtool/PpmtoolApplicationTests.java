a9682a369edff7490c090a58985a88735d7bf87a
trusu <tiberiu.rusu@expleogroup.com>
2020-03-12 13:06:57 +0200

    Created Project entity, Added ProjectRepository

--- ppmtool/ppmtool/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
+++ ppmtool/ppmtool/src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -1,4 +1,4 @@
-package io.agileintelligence.ppmtool;
+package test.java.io.agileintelligence.ppmtool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
